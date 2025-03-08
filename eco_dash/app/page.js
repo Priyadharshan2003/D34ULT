2c5d137bc85da16b4326d0fc5ffadb76e20d0f82
Abinanthan47 <abinanthan247@gmail.com>
2025-03-08 19:00:38 +0530

    backend schema

--- eco_dash/app/page.js
+++ eco_dash/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-5xl text-center text-red-500">Hello world</h1>
-   </div>
+    <div>
+      <Hero />
+    </div>
   );
 }
--- eco_dash/components/Header.jsx
+++ eco_dash/components/Header.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 import Image from "next/image";
-
+ 
 const Header = async () => {
   
 
